e1ae08f08806fa694024bdc5e7bc73d9459fa25c
Rohit Rajaram Kagankar <112383067+Rohitkagankar@users.noreply.github.com>
2024-02-21 23:46:55 +0530

    image generation

--- script.js
+++ script.js
@@ -1,19 +1,63 @@
 const generateForm=document.querySelector(".generate-form");
 const imageGallery=document.querySelector(".image-gallery");
 
+const OPEN_API_KEY = "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";
+let isImageGenerating = false;
+
+const updateImageCard = (imgDataArray) => {
+    imgDataArray.forEach((imgObject,index)=>{
+        const imgCard = imageGallery.querySelectorAll(".img-card")[index];
+        const imgElement = imgCard.querySelector("img");
+        const downloadBtn = imgCard.querySelector(".download-btn");
+
+        //set the image source to the AI
+        const aiGeneratedImg =`data:image/jpeg;base64,${imgObject.b64_json}`;
+        imgElement.src = aiGeneratedImg;
+
+        //when the image is loaded , remove the loading class
+        imgElement.onload = () =>{
+            imgCard.classList.remove("loading");
+            downloadBtn.setAttribute("href",aiGeneratedImg);
+            downloadBtn.setAttribute("download",`${new Date().getTime().jpg}`);
+        }
+    })
+}
+
 
 const generateAiImage = async (userPrompt,userImgQuantity) =>{
     try{
-        const response = await fetch("")
+        //send a request to the openAI API to generate images based on user inputs
+        const response = await fetch("https://api.openai.com/v1/images/generations",{
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${OPEN_API_KEY}`
+            },
+            body: JSON.stringify({
+                prompt:userPrompt,
+                n: parseInt(userImgQuantity),
+                size: "512x512",
+                response_format: "b64_json"
+            })
+        });
+        if(!response.ok) throw new Error("Failed to generate images! Please try again.");
+
+        const {data}=await response.json(); //get data from the response
+        updateImageCard([...data]);
+        // console.log(data);
     }catch(error){
-        console.log(error);
+        alert(error.message);
+    }finally{
+        isImageGenerating = false;
     }
 }
 
 
 const handleFormSubmission=(e) =>{
     e.preventDefault();
-    
+    if(isImageGenerating) return;
+    isImageGenerating = true;
+
     //get user input and image quantity from the form
     const userPrompt =e.srcElement[0].value;
     const userImgQuantity = e.srcElement[1].value;
